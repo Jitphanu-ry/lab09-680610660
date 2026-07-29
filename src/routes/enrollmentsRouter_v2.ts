898a2750e31cd9b5b5ae3e217c4a890384cc9a2f
Jitphanu-ry <jitphanuriyasarn@gmail.com>
2026-07-29 17:07:03 +0700

    push

--- src/routes/enrollmentsRouter_v2.ts
+++ src/routes/enrollmentsRouter_v2.ts
@@ -58,7 +58,7 @@ router.get("/", (req: Request, res: Response) => {
       // return all users
         return res.json({
           success: true,
-          data: enrollments,
+          enrollments: enrollments,
       });
       } catch (err) {
         return res.status(500).json({
@@ -70,7 +70,7 @@ router.get("/", (req: Request, res: Response) => {
     }
     //student see only youself
     if(user.role === "STUDENT"){
-      const findstudent = enrollments.filter((e)=>e.studentId === user.studentId)
+      const findstudent = enrollments.filter((e)=>e.studentId === user.studentId).map((e)=>courses.find((c)=>c.courseId === e.courseId))
       return res.status(200).json({
         success:true,
         data:findstudent
@@ -81,48 +81,98 @@ router.get("/", (req: Request, res: Response) => {
 
 // POST /api/v2/student/enroll
 router.post("/", (req: Request, res: Response) => {
-  // 1. get username and password from body
-  const { username,password} = req.body;
-  const user = users.find((u)=> u.username === username && u.password === password);
+  // 1. get enroll from body
+  const enroll = req.body as Enrollment;
+  const authHeader = req.headers["authorization"];
 
-  // 2. check if user exists (search with username & password in DB)
-  if(!user){
-    return res.status(404).json({
-        success: false,
-        message: "Invalid username or password"
-    })
+  //check auth
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({
+      success: false,
+      message: "Authorization header is required",
+    });
   }
-  //not allow admin
-  if(user.role === "ADMIN"){
-    return res.status(403).json({
+
+  console.log(authHeader);
+  const token = authHeader.split(" ")[1];
+  //check token
+  if (token === null) {
+    return res.status(401).json({
+      success: false,
+      message: "token is required",
+    });
+  }
+  
+  const jwt_secret = process.env.JWT_SECRET || "this_is_my_secret";
+  jwt.verify(token, jwt_secret, (err, payload) => {
+    if (err) {
+      return res.status(403).json({
+        success: false,
+        message: "Invalid or expired token",
+      });
+    }
+
+    //find payload
+    const user_payload = payload as UserPayload;
+    const user = users.find((u) => u.username === user_payload.username);
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized user",
+      });
+    }
+    //not allow admin
+    if(user.role === "ADMIN"){
+      return res.status(403).json({
         success: true,
         message: "Only Student can access this API route"
     })
-  }
-  // 3. create JWT token (with user info object as payload) using JWT_SECRET_KEY
-  const jwt_secret = process.env.jwt_secret || "this_is_my_secret";
-  const token = jwt.sign(
-    {
-        //app payload
-        username: user.username,
-        studentId: user.studentId,
-        role: user.role
-    },
-    jwt_secret,
-    {expiresIn:"30m"}
-)
-  //    (optional: save the token as part of User data)
+    }
 
-  // 4. send HTTP response with JWT token
-  return res.status(200).json({
-    success: true,
-    message: "Login successful",
-    token: token
+    if(user.role === "STUDENT"){
+      if(enroll.courseId && enroll.studentId){
+      const findIndex = enrollments.findIndex((e)=>e.courseId === enroll.courseId && e.studentId === enroll.studentId);
+      const student = students.find((s)=>s.studentId === enroll.studentId);
+      
+      //check student str
+      if(enroll.studentId.length !== 9){
+        return res.status(400).json({
+          success: false,
+          message: "Student Id must contain 9 characters"
+        })
+      }
+      //check course str
+      if(enroll.courseId.length !== 6){
+        return res.status(400).json({
+          success: false,
+          message: "Course Id must contain 6 characters"
+        })
+      }
+
+      //check student user
+      if(enroll.studentId !== user.studentId){
+        return res.status(400).json({
+          success: false,
+          message: "you can't enroll other student course"
+        })
+      }
+
+      if(findIndex === -1 && student){
+        const add_Enroll = enrollments.push(enroll);
+        const add_Course = student.courses?.push(enroll.courseId);
+        return res.status(200).json({
+          success: true,
+          message: "You has been enrolled from this course."
+      })}else{
+        return res.status(404).json({
+          success: false,
+          message: "you already enroll this course"
+        })
+      }
+    }
+    }
   })
-  return res.status(500).json({
-    success: false,
-    message: "POST /api/v2/enrollment/login has not been implemented yet",
-  });
 });
 
 router.delete("/", (req: Request, res: Response) => {
@@ -206,7 +256,7 @@ router.delete("/", (req: Request, res: Response) => {
         const delete_Enroll = enrollments.splice(findIndex,1);
         const courseidx = student.courses?.findIndex((c)=>c=== enroll.courseId);
         if(courseidx){
-          const delete_Course = student.courses?.splice(courseidx,0);
+          const delete_Course = student.courses?.splice(courseidx,1);
         } 
         return res.status(200).json({
           success: true,
